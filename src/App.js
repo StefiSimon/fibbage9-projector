e36a0a93117f64b68e422cf6b651f39eb3a095ee
Stefi Simon <stefisimon@roser-ict.com>
2019-07-20 11:50:52 +0300

    add start page and firebase setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PincodeStartPage from '../src/pages/PincodeStartPage/PincodeStartPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="projector-header">
+          Projector app header
+        </header>
+        <main>
+          <Route exact path="/" component={PincodeStartPage} />
+        </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/PincodeStartPage/PincodeStartPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PincodeStartPage extends Component {
+  render() {
+    return (
+      <div>pincode start page</div>
+    )
+  }
+}
+
+export default PincodeStartPage;
